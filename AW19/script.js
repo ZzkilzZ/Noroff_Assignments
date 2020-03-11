1adf608cc9ceac47b4e811e8125c0a1381a1095d
Marek Fischer <mfischer@127.0.0.53>
2020-03-11 17:31:11 +0000

    did muh homework, tested with node

---
+++ AW19/script.js
@@ -0,0 +1,45 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+console.log(petArray[1]);
+
+// question 3
+petArray.push("sheep");
+
+// question 4
+console.log(petArray.length);
+
+// question 5
+var catObject = {
+	name: "Chacha",
+	colour: "white",
+	age: 15
+};
+
+// question 6
+console.log(catObject.age);
+
+// question 7
+var catArray = [{
+	name: "Chacha",
+	colour: "white",
+	age: 15
+}];
+
+// question 8
+catArray.forEach( (item) => {
+	Object.keys(item).forEach((property) => {
+		console.log(property);
+	})
+})
+
+// question 9
+function logToConsole(logItem)
+{
+	console.log(logItem);
+}
+
+// question 10
+
+logToConsole("donkey");
